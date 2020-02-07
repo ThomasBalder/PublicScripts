3db2731e0900961fa6de2901324a99e0e2e7a144
Lesilhouette <56297954+ThomasBalder@users.noreply.github.com>
2020-02-07 12:42:24 +0100

    Added correct remark expression

--- Miscellaneous/Change Office update channel.bat
+++ Miscellaneous/Change Office update channel.bat
@@ -9,11 +9,13 @@ reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Office\16.0\Common\Off
 :End 
 Endlocal
 
-#
-# Udate channels
-# Monthly Channel (Targeted): CDNBaseUrl = http://officecdn.microsoft.com/pr/64256afe-f5d9-4f86-8936-8840a6a4f5be
-# Semi-Annual Channel (Targeted):CDNBaseUrl = http://officecdn.microsoft.com/pr/b8f9b850-328d-4355-9145-c59439a0c4cf
-# Monthly Channel: CDNBaseUrl = http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60
-# Semi-Annual Channel: CDNBaseUrl = http://officecdn.microsoft.com/pr/7ffbc6bf-bc32-4f92-8982-f9dd17fd3114
-#
+@Echo Please manually update Office to download the new buildversion.
+
+REM
+REM Udate channels
+REM Monthly Channel (Targeted): CDNBaseUrl = http://officecdn.microsoft.com/pr/64256afe-f5d9-4f86-8936-8840a6a4f5be
+REM Semi-Annual Channel (Targeted):CDNBaseUrl = http://officecdn.microsoft.com/pr/b8f9b850-328d-4355-9145-c59439a0c4cf
+REM Monthly Channel: CDNBaseUrl = http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60
+REM Semi-Annual Channel: CDNBaseUrl = http://officecdn.microsoft.com/pr/7ffbc6bf-bc32-4f92-8982-f9dd17fd3114
+REM
 
